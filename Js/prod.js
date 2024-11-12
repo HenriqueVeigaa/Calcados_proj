02fc6ad1e8e72f55baa943c683f6954450fbfaea
Henrique <henrique.messias@etec.sp.gov.br>
2024-11-12 17:58:04 -0300

    Conclusão do carrinho de compras

--- Js/prod.js
+++ Js/prod.js
@@ -1,62 +1,66 @@
- if(document.readyState == "loading"){
-    document.addEventListener("DOMContentLoaded", ready)
- }else{
+if(document.readyState == "loading"){                          //verifica se a página html está carregando ou está totalmente carregada
+document.addEventListener("DOMContentLoaded",ready)           //DOMContentLoaded indica quando os elementos html foram carregados; 
+}else{
     ready()
- }
+}
+                               
+function ready(){
 
- function ready() {
-    const Remove_btns = document.getElementsByClassName("remove-btn")
+    const removeProdBtn =document.getElementsByClassName("remove-btn")      //pegando elementos q possuem a classe remove-btn
+    for(var i=0;i<removeProdBtn.length;i++){
+    removeProdBtn[i].addEventListener("click",removeProd)                   //pegando a quantidade de botoes da classe com o for, atribuindo um ouvidor de eventos ao clicar capturando a ação do botao                                                    
+}
 
-    for(var i=0;i< Remove_btns.length;i++){
-       Remove_btns[i].addEventListener("click", removeProd)
-          
-       }
+const QtdInput = document.getElementsByClassName("qtd-prod")
+for( var i = 0; i< QtdInput.length; i++){
+QtdInput[i].addEventListener("change",CheckInptNull) //o addEventListener "olha" mudanças nos botões de input e com o change verifica a qtd dos produtos e atualiza o valor com a função 
+}
 
-const qtdInput = document.getElementsByClassName("qtd-prod")
-for (var i =0; i< qtdInput.length;i++){
-qtdInput[i].addEventListener("change",updateTotal)
+const AddCartBtn = document.getElementsByClassName("input-btn")
+for(var i = 0; i< AddCartBtn.length; i++){
+AddCartBtn[i].addEventListener("click",AddProdCart)
+}
 
 }
 
-const addToCart =document.getElementsByClassName("btn")
-for(var i=0; i< addToCart.length;i++) {
-    addToCart[i].addEventListener("click", addProdToCart)
-}
- }
+function CheckInptNull(event){       //checando se o input é zero para removê-lo do carrinho; //capturar o input que sofreu alteração
+  if(event.target.value== "0"){         //condição pra checar se o valor do input é zero
 
- function checkIfInputNull(event){
-if(event.target.value =="0"){
     event.target.parentElement.parentElement.remove()
-}
- }
+  }                                
 
-function addProdToCart(event) {
-    const button = event.target
-    const prodInfo = button.parentElement.parentElement
-    const prodImg = prodInfo.getElementsByClassName("prod-img")[0].src
-    const prodTitle = prodInfo.getElementsByClassName("shoe-title")
-    const prodPrice = prodInfo.getElementsByClassName("shoe-price")[0].innerText
-
-
-const prodCartName = document.getElementsByClassName("shoe-title")
-for(var i =0; i< prodCartName.length;i++){
-    if(prodCartName[i].innerText == prodTitle){
-        prodCartName[i].parentElement.parentElement.getElementsByClassName("qtd-prod")[0].value++
-        return
-    }
+    updateValor();
 }
 
 
-     let newCartProd = document.createElement("tr")
-     newCartProd.classList.add("cart-prod")
+function AddProdCart(event){    //função criada para pegar os 3 elementos necessários para adicionar-los no carrinho
+    const btn = event.target //é retornado o btn clicado
+    const ProdInfo= btn.parentElement //Criado a variavel ProdInfo onde é retornado a classe pai do btn(que no código é a div shoes)
+    const ProdImg = ProdInfo.getElementsByClassName("shoe-image")[0].src //variavel que quando o botão é clicado retorna a imagem e o src, que irá aparecer no carrinho
+    const ProdNome = ProdInfo.getElementsByClassName("shoe-title")[0].innerText //variavel retorna a tag html da classe shoe-title, mas com o InnerText se busca o nome do produto
+    const ProdPrice = ProdInfo.getElementsByClassName("shoe-price")[0].innerText 
 
-     newCartProd.innerHTML = `
-      <td class="prod-id">
-                        <img src="${prodImg}" class="cart-img" alt="${prodTitle}">
-                        <strong class="cart-title">${prodTitle}</strong>
+    const ProdCartName = document.getElementsByClassName("cart-title") //verificando se o produto que vamos colocar no carrinho já está nele, para apenas aumentar a qtd de inputs
+    for(var i =0; i < ProdCartName.length;i++){                        //verificando se o produto que vamos colocar no carrinho já está nele, para apenas aumentar a qtd de inputs
+      if( ProdCartName[i].innerText === ProdNome){
+       ProdCartName[i].parentElement.parentElement.getElementsByClassName("qtd-prod")[0].value++ //pegando o elemento pai do prodcartname e atribuindo mais um ao input
+    return //o return faz com que não execute o NewCartProd evitando que o produto seja adicionado novamente, aumentando somente a qtd de inputs
+    }                           
+   
+}
+
+   let NewCartProd = document.createElement("tr")        //criando um elemento tr js
+    NewCartProd.classList.add("cart-prod")                        //Acessando as classes dos elementos com o classlist e adicionando a classe do tr com o add
+    
+    NewCartProd.innerHTML =                                        //informando o que vai ter dentro do tr com o innerhtml
+
+    `
+          <td class="prod-id">
+                        <img src="${ProdImg}" class="cart-img" alt="${ProdNome}">
+                        <strong class="cart-title">${ProdNome}</strong>
                     </td>
                     <td class="cart-price">
-                        <span>${prodPrice}</span>
+                        <span>${ProdPrice}</span>
                     </td>
 
                     <td>
@@ -64,34 +68,45 @@ for(var i =0; i< prodCartName.length;i++){
                         <button class="remove-btn" type="button">Remover</button>
 
                     </td>
-     `
-   const tableBody =  document.querySelector("cart-table tbody")
-   tableBody.append(newCartProd)
-   updateTotal()
-   newCartProd.getElementsByClassName("qtd-prod")[0].addEventListener("change", checkIfInputNull)
-   newCartProd.getElementsByClassName("remove-btn")[0].addEventListener("click",removeProd)
+    
+    `
+                
+
+
+   const Table= document.querySelector(".cart-table tbody")              //criando a váriavel Table pra pegar o tbody
+   Table.append(NewCartProd)                                           //append é responsável por adicionar o elemento tr criado no tbody
+    updateValor()
+   NewCartProd.getElementsByClassName("qtd-prod")[0].addEventListener("change",CheckInptNull)                 //pegando o input para atualizar o valor depois de um produto ser acrescentado no carrinho
+    NewCartProd.getElementsByClassName("remove-btn")[0].addEventListener("click",removeProd)                //pegando o input para remove-lo  depois de um produto ser acrescentado no carrinho
+}
+
+
+function removeProd(event){
+    event.target.parentElement.parentElement.remove() //acessando o elemento pai e removendo a tag tr para o produto desaparecer do carrinho
+    updateValor() //após remover os produtos, a função altera o valor
 }
 
 
 
 
- function removeProd(event) {
-    event.target.parentElement.parentElement.remove()
-    updateTotal()
+function updateValor(){
 
- }
+    let TotalCart = 0; //variavel responsavel pra armazenar o valor total do carrinho
 
- function updateTotal(){
-    let Total = 0
+const cartProd = document.getElementsByClassName("cart-prod")     //pegando os itens do carrinho com a classe cart-prod
+for( var i = 0; i< cartProd.length; i++){
+    const prodPrice = cartProd[i].getElementsByClassName("cart-price")[0].innerText.replace("R$", "").replace(",", ".")    //replace substitui o valor do primeiro parametro pelo segundo pra fazer a multiplicação           //innertext pega o texto da tag html, e nao a tag em si;          //pegando o preço do produto dos itens do carrinho
+    const prodQtd = cartProd[i].getElementsByClassName("qtd-prod")[0].value             //Procura a classe qtd-prod no cartprod(item do carrinho) e acessa a posição zero para retornar o valor dos inputs com o value
 
-    const cartProd = document.getElementsByClassName("cart-prod")
-    for(var i = 0; i< cartProd.length;i++){
-       const prodPrice = cartProd[i].getElementsByClassName("cart-prod")[0].innerHTML.Text.replace("R$", "").replace(",", ".")
-       const prodQtd = cartProd[i].getElementsByClassName("qtd-prod")[0].value
-       Total +=  prodPrice * prodQtd
-   }
-       Total= Total.toFixed(2)
-       Total = Total.replace(".", ",")
-       document.querySelector(".cart-total span").innerText = "R$"+ Total  
- }
+TotalCart +=  prodPrice * prodQtd //+= significa que recebe ele mesmo mais a condição imposta
+
+}    
+TotalCart = TotalCart.toFixed(2)
+TotalCart = TotalCart.replace(".", ",")
+document.querySelector(".cart-total span").innerText = "R$"+ TotalCart  
+ //outra forma de pegar elementos em js, elemento span da classe cart-total    
+    
+}
+
+
 
